1103435e36e7b96d81f0918f35bafed48f0cfd4e
Michael Gertz <michael@ringhus.dk>
2023-03-01 10:55:36 +0100

    Copied from AsteroidsEntityFramework

---
+++ Asteroids/Core/src/main/java/dk/sdu/student/miger20/main/Main.java
@@ -0,0 +1,22 @@
+package dk.sdu.student.miger20.main;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		LwjglApplicationConfiguration cfg =
+			new LwjglApplicationConfiguration();
+		cfg.title = "Asteroids";
+		cfg.width = 800;
+		cfg.height = 450;
+		cfg.useGL30 = false;
+		cfg.resizable = true;
+		
+		new LwjglApplication(new Game(), cfg);
+		
+	}
+	
+}
